c5f79d0f01199ec4ee5717e0173bae00524712c3
McLavish <Luca.renna.studenti@isii.it>
2020-05-03 20:30:03 +0200

    Revert "Revert "added exec to Mongoose queries for better stacktraces""
    This reverts commit 784c66fdc3b2d8b42f2d5362712b80db46b2afea.

--- core/startup/session_init.js
+++ core/startup/session_init.js
@@ -6,13 +6,13 @@ const SessionDocument = require("../classes/session_document");
 //For each new user a session is created
 module.exports = async (ctx) => {
     //Load the user's saved conversation from the DB
-    let mongoUser = await User.findOne({"id":ctx.message.from.id});
+    let mongoUser = await User.findOne({"id":ctx.message.from.id}).exec();
 
     if (mongoUser == null)
         throw new Error("User not found! Database might be corrupted");
 
     let chat = await ctx.getChat();
-    let mongoChat = await Chat.findOne({"id":chat.id});
+    let mongoChat = await Chat.findOne({"id":chat.id}).exec();
 
     if(mongoChat == null)
         throw new Error("Chat not found! Database might be corrupted");
